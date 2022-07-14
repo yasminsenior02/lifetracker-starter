54945e841f9d677b373709de210a4eed24abbf63
yasminsenior02 <yasminsenior02@gmail.com>
2022-07-14 16:09:52 -0700

    porting running

--- lifetime_api/routes/nutrition.js
+++ lifetime_api/routes/nutrition.js
@@ -5,14 +5,32 @@ const { createUserJwt } = require("../utils/tokens");
 const security = require("../security");
 const router = express.Router();
 
-router.get("/", security.requireAuthenticatedUser, async (req, res, next) => {
-  try {
-    var { user } = res.locals.user;
-    const nutrits = await Nutrition.listNutrition(user);
-    return res.status(200).json({ nutrition: nutrits });
-  } catch (err) {
-    next(err);
+router.get(
+  "/nutrition",
+  security.requireAuthenticatedUser,
+  async (req, res, next) => {
+    try {
+      var { user } = res.locals.user;
+      const nutrits = await Nutrition.listNutrition(user);
+      return res.status(200).json({ nutrition: nutrits });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
+
+router.post(
+  "/nutrition",
+  security.requireAuthenticatedUser,
+  async (req, res, next) => {
+    try {
+      var { user } = res.locals.user;
+      const nutrits = await Nutrition.listNutrition(user);
+      return res.status(200).json({ nutrition: nutrits });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
